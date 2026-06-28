fedf7d50e7392e34986ef5e098c678ab37f4e198
keerthirani774-alt <keerthirani774@gmail.com>
2026-06-28 16:59:38 +0530

    Add files via upload

---
+++ house detalis/house.py
@@ -0,0 +1,8 @@
+# House Price Prediction
+
+area = int(input("Enter area (sq.ft): "))
+hall = int(input("Enter number of halls: "))
+
+price = (area * 5000) + (hall * 100000)
+
+print("Predicted House Price =", price)
